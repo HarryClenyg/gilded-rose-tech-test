4bb5a04ef1fad68eb8b2ec691fbe395600e482fc
Harry <harrydawson90@hotmail.co.uk>
2022-04-28 12:58:36 +0100

    passed test for aged brie quality increases each day

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,18 +1,25 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
 describe("Gilded Rose", () => {
-  const gildedRose = new Shop([new Item("+5 Dexterity Vest", 10, 20)]);
-  const items = gildedRose.updateQuality();
+  const gildedRose = new Shop();
+  const items = gildedRose.items;
+  items.push(new Item("+5 Dexterity Vest", 10, 20));
+  items.push(new Item("Aged Brie", 2, 0));
+  const updateItems = gildedRose.updateQuality();
 
   it("should add new item to shop's item list", () => {
-    expect(items[0].name).toBe("+5 Dexterity Vest");
+    expect(updateItems[0].name).toBe("+5 Dexterity Vest");
   });
 
   it("should reduce quality by 1 each time updateQuality is called (as if the end of the day)", () => {
-    expect(items[0].quality).toBe(19);
+    expect(updateItems[0].quality).toBe(19);
   });
 
   it("should reduce sellIn by 1 each time updateQuality is called (as if the end of the day)", () => {
-    expect(items[0].sellIn).toBe(9);
+    expect(updateItems[0].sellIn).toBe(9);
+  });
+
+  it("should increase quality of aged brie each day", () => {
+    expect(updateItems[1].quality).toBe(1);
   });
 });
