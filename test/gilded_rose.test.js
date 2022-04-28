cede67536bd8597ec893fd3a6e37f7999de59e74
Harry <harrydawson90@hotmail.co.uk>
2022-04-28 12:34:12 +0100

    passed tests for reducing quality by 1 and sellIn by one each time updateQuality is called (representing the end of the day)

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,9 +1,18 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
-describe("Gilded Rose", function() {
-  it("should add new item to shop's item list", function() {
-    const gildedRose = new Shop([new Item("+5 Dexterity Vest", 10, 20)]);
-    const items = gildedRose.updateQuality();
+describe("Gilded Rose", () => {
+  const gildedRose = new Shop([new Item("+5 Dexterity Vest", 10, 20)]);
+  const items = gildedRose.updateQuality();
+
+  it("should add new item to shop's item list", () => {
     expect(items[0].name).toBe("+5 Dexterity Vest");
+  });
+
+  it("should reduce quality by 1 each time updateQuality is called (as if the end of the day)", () => {
+    expect(items[0].quality).toBe(19);
+  });
+
+  it("should reduce sellIn by 1 each time updateQuality is called (as if the end of the day)", () => {
+    expect(items[0].sellIn).toBe(9);
   });
 });
